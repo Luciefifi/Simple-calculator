b9cd5c1f14448c349c8cd228d311dc1993d48134
Luciefifi <niyomutonilucie@gmail.com>
2022-09-14 10:38:05 +0200

    added index.html , styles.css , calc.js files

---
+++ calc.js
@@ -0,0 +1,15 @@
+(function(){
+    let screen = document.querySelector('.screen');
+    let buttons  = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-delete');
+    let equal =  document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+            let value = e.target.dataset.num;
+        screen.value += value;        })
+    })
+}) ();
+
+
+
