eb4353cf36d6c3536d7447966d973109fad0921e
Luciefifi <niyomutonilucie@gmail.com>
2022-09-15 11:08:57 +0200

    modified index.html file

--- calc.js
+++ calc.js
@@ -2,14 +2,29 @@
     let screen = document.querySelector('.screen');
     let buttons  = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-delete');
-    let equal =  document.querySelector('.btn-equal');
+    let Equal =  document.querySelector('.btn-equal');
 
     buttons.forEach(function(button){
         button.addEventListener('click', function(e){
             let value = e.target.dataset.num;
         screen.value += value;        })
     })
-}) ();
+})
+Equal.addEventListener('click', function(e)
+{
+  if(screen.value === "")
+  {
+   screen.value = ""; 
+  } else {
+    let answer = eval(screen.value);
+    screen.value = answer;
+  } 
+})
+clearInterval.addEventListener('click', function(e)
+{
+    screen.value = "";
+})
+();
 
 
 
